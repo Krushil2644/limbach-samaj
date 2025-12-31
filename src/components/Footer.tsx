2e4a471a3a9dcfc391f56c4cc25236f6f39d8f88
Krushil2644 <158112666+Krushil2644@users.noreply.github.com>
2025-12-31 13:32:46 -0500

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -29,6 +29,7 @@ export default function Footer() {
             </div>
 
             {/* Social Links with enhanced styling */}
+            {siteConfig.showFooterSocialLinks && (  //temporary hidden
             <div>
               <p className="text-xs font-semibold text-muted-foreground uppercase tracking-wider mb-2.5">
                 Connect With Us
@@ -69,6 +70,8 @@ export default function Footer() {
                 )}
               </div>
             </div>
+            )}
+            
           </div>
 
           {/* Quick Links */}
